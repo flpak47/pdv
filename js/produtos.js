09399685423f84bf7839f848f9e96f27a4bbce37
Fabio <flp_padilha@hotmail.com>
2018-04-23 21:50:13 -0300

    Modal add produtos

--- js/produtos.js
+++ js/produtos.js
@@ -1,5 +1,7 @@
 $(function(){
 
+    $('#preco').mask('000,00', {reverse: true});
+
     $.getJSON('/model/listar-produtos.php', function(dados){
 
         dados.forEach(function(el, id){      
@@ -8,20 +10,59 @@ $(function(){
             + '<td>'+ el.id +'</td>'
             + '<td>'+ el.nome +'</td>'
             + '<td>'+ el.categoria +'</td>'
-            + '<td>'+ el.preco +'</td>'
+            + '<td>R$ '+ el.preco +'</td>'
             + '<td>'
-                + '<a href="/produtos-alterar.html" class="btn btn-primary" title="Editar"><i class="fas fa-edit"></i> </a>'
-                + '<a href="/produtos-deletar.html" class="btn btn-danger" title="Deletar"><i class="fas fa-minus-circle"></i> </a>'
+            + '<a href="/produtos-alterar.html" class="btn btn-primary" title="Editar"><i class="fas fa-edit"></i></a>'
+            + '&nbsp'
+            + '&nbsp'
+            + '<a href="#" class="btn btn-danger" data-toggle="modal" data-target="#deletar-produto" title="Deletar"><i class="fas fa-minus-circle"></i> </a>'
             + '</td>'
-        + '</tr>';
+            + '</tr>';
 
             $('#lista-produtos').append(tr);
 
 
-        });
+        });// forEach
 
+
+    });// getJSON
+
+    $("#salvar-produto").click(function(){
+        
+        
+        if ($('#nome').val().length <= 0){
+            $('#nome').addClass('is-invalid');
+            return false;
+        }
+    });
+
+    $("#salvar-produto").click(function(){
+        
+        
+        if ($('#marca').val().length == 0){
+            $('#marca').addClass('is-invalid');
+            return false;
+        }  
 
     });
+        
+        $("#salvar-produto").click(function(){
+        
+        
+            if ($('#categoria').val().length == 0){
+                $('#categoria').addClass('is-invalid');
+                return false;
+            }                  
+    });
+
+    $("#salvar-produto").click(function(){
+        
+        
+        if ($('#preco').val().length <= 0){
+            $('#preco').addClass('is-invalid');
+            return false;
+        }                  
+});
 
 
 });
